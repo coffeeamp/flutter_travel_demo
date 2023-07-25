c0b464ffc3ce44724cdf290b0d47c1a8a04e4e90
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2023-07-25 21:31:34 +0900

    utf8decode

--- lib/student.dart
+++ lib/student.dart
@@ -5,9 +5,9 @@ class Student{
   String baseUrl = "http://10.0.2.2:8000/my_app/student/";
   Future<List> getAllStudent() async{
     try {
-      var responese = await http.get(Uri.parse(baseUrl));
+      var responese = await http.get(Uri.parse(baseUrl),headers: {'Accept-Charset': 'utf-8',});
       if (responese.statusCode == 200) {
-        return json.decode(responese.body);
+        return json.decode(utf8.decode(responese.bodyBytes));
       } else {
         return Future.error("Server error");
       }
