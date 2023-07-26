ff90c4760ba07bed48ff5858b4552a98f708e374
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2023-07-27 00:14:23 +0900

    post

---
+++ lib/post/post.dart
@@ -0,0 +1,21 @@
+
+class Post{
+  final String title;
+  final String content;
+
+  Post ({required this.title, required this.content});
+
+  factory Post.fromJson(Map<String, dynamic> json){
+    return Post(
+      title: json['title'],
+      content: json['content'], 
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'title': title,
+      'content': content,
+    };
+  }
+}
