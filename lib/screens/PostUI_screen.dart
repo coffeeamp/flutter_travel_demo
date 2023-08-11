70a07c1291ff10d537b0cf279c533e6fa8e5e804
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2023-08-11 17:22:11 +0900

    post_4

--- lib/screens/PostUI_screen.dart
+++ lib/screens/PostUI_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_demo/post/post.dart';
 import 'package:flutter_travel_demo/providers/post_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -22,14 +23,19 @@ class PostListScreen extends StatelessWidget {
               return Center(child: Text('An error occurred!'));
           }else{
             return Consumer<PostProvider>(
-              child: Center(child: const Text('Got no posts yet')),
-              builder: (ctx, postProvider, ch) => postProvider.posts.length <= 0
-              ? ch!
-              : ListView.builder(
-                itemCount: postProvider.posts.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  title: Text(postProvider.posts[i].title),
-                  subtitle: Text(postProvider.posts[i].content),
+              builder: (ctx, postProvider, ch){
+                List<Post> posts = postProvider.posts;
+                return ListView.builder(
+                  itemCount: posts.length,
+                  itemBuilder:(context, index){
+                    Post post = posts[index];
+                    return ListTile(
+                      title: Text(post.title),
+                      subtitle: Text(post.content),
+                    );
+                  },
+                );
+              }
                 ),
               ),
             );
