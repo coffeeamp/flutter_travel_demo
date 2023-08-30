db1f6deb98cbd4533c6315e6e100653c9a4b3cbc
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2023-08-30 22:55:03 +0900

    post_todo

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_travel_demo/providers/post_provider.dart'; // 위에서 정의한 PostProvider 클래스 파일
 import 'package:flutter_travel_demo/screens/lobby_screen.dart';
 import 'package:flutter_travel_demo/screens/main_screen.dart';
 
@@ -15,22 +14,19 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => PostProvider(),
-      child: MaterialApp(
-        title: 'travel app',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(), // 로그인이 되어있는지 확인
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return LobbyScreen();
-            }
-            return LoginSignupScreen();
-          },
-        ),
+    return MaterialApp(
+      title: 'travel app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(), // 로그인이 되어있는지 확인
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return LobbyScreen();
+          }
+          return LoginSignupScreen();
+        },
       ),
     );
   }
